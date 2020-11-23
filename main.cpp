6e3fa43371053be6a4bb88115e2f95781cd6618b
mukeshbbx <74864792+mukeshbbx@users.noreply.github.com>
2020-11-23 22:05:34 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int a;
+   cout <<"Enter the Number :\n";
+   cin>>a;
+   cout <<"Displaying the number given by the user :\n"<<a;
+    return 0;
+}
